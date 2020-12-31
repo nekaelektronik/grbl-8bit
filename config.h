de57168c6d948835ee8361b4c9f414a1a816f303
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-31 15:06:47 +0300

    Change axis number

--- config.h
+++ config.h
@@ -50,8 +50,8 @@
 // Axis array index values. Must start with 0 and be continuous.
 #ifdef DEFAULTS_RAMPS_BOARD
   // 4, 5 & 6 axis support only for RAMPS 1.4 (for the moment :-)...)
-  #define N_AXIS 3            // Number of axes
-  #define N_AXIS_LINEAR 3     // Number of linears axis
+  #define N_AXIS 6            // Number of axes
+  #define N_AXIS_LINEAR 6     // Number of linears axis
 #else
   #define N_AXIS 3 // Number of axes = 3 if not DEFAULTS_RAMPS_BOARD
 #endif
